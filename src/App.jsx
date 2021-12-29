97365efb1c54f4717030068aff930a1bd109c379
BlackH3art <jaroslaw.musielak@outlook.com>
2021-12-29 16:26:28 +0100

    basic file/folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import './App.css'
 
+// components
+import Welcome from './components/Welcome/Welcome';
+import Navbar from './components/Navbar/Navbar';
+import Services from './components/Services/Services';
+import Transactions from './components/Transactions/Transactions';
+import Footer from './components/Footer/Footer';
+
 const App = () => {
 
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      
+      <Services />
+      <Transactions />
+      <Footer />
+
     </div>
   )
 }
